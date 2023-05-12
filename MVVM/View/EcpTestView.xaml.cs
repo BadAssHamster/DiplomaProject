008a5cf731c8164df522aa40e8ee197fc7901d49
Денис Шолопин <denis.sholopin@yandex.ru>
2023-05-12 15:09:59 +0300

    Появляется первый вопрос метод NextQuestion

--- MVVM/View/EcpTestView.xaml.cs
+++ MVVM/View/EcpTestView.xaml.cs
@@ -20,7 +20,7 @@ namespace DiplomaProject.MVVM.View
     public partial class EcpTestView : UserControl
     {
         public int TestIndex = 1;
-        public int questionCounter = 0;
+        public int questionCounter = -1;
         public List<(string question, int questionId, int questionType)> Questions = new List<(string questionList, int questionId, int questionType)>();
         public List<(string answer, int checkAnswer)> Answers = new List<(string answer, int checkAnswer)>();
 
@@ -58,6 +58,7 @@ namespace DiplomaProject.MVVM.View
 
         public List<(string answer, int checkAnswer)> GetAnswers(List<(string answer, int checkAnswer)> answers, int questionId)
         {
+            Answers.Clear();
             string connectionString = "server=localhost;port=3307;user id=root;password=jdqVM74kzuvu3I0w;database=programdb";
             MySqlConnection connection = new MySqlConnection(connectionString);
 
@@ -65,7 +66,7 @@ namespace DiplomaProject.MVVM.View
             {
                 connection.Open();
 
-                MySqlCommand command = new MySqlCommand($"SELECT answer, check_answer FROM answers WHERE idQuestion={questionId}", connection);
+                MySqlCommand command = new MySqlCommand($"SELECT answer, checkAnswer FROM answers WHERE idQuestion={questionId}", connection);
                 MySqlDataReader reader = command.ExecuteReader();
 
                 while (reader.Read())
@@ -88,10 +89,68 @@ namespace DiplomaProject.MVVM.View
         }
         public void NextQuestion()
         {
+            questionCounter++;
             var question = Questions[questionCounter];
             QuestionText.Text = question.question.ToString();
-            
+            switch (question.questionType)
+            {
+                case 1:
+                    RdbStack.Visibility = Visibility.Visible;
+                    CheckStack.Visibility = Visibility.Collapsed;
+                    TextStack.Visibility = Visibility.Collapsed;
+                    break;
+                case 2:
+                    RdbStack.Visibility = Visibility.Collapsed;
+                    CheckStack.Visibility = Visibility.Visible;
+                    TextStack.Visibility = Visibility.Collapsed;
+                    break;
+                case 3:
+                    RdbStack.Visibility = Visibility.Collapsed;
+                    CheckStack.Visibility = Visibility.Collapsed;
+                    TextStack.Visibility = Visibility.Visible;
+                    break;
+            }
 
+            GetAnswers(Answers, question.questionId);
+            
+            if (question.questionType != 3)
+            {
+                for (int answerNumber = 0; answerNumber < 4; answerNumber++)
+                {
+                    var answer = Answers[answerNumber];
+                    switch (question.questionType)
+                    {
+                        case 1:
+                            var find_rdb = RdbStack.FindName($"rdb_{answerNumber + 1}") as RadioButton;
+                            if (find_rdb != null)
+                            {
+                                find_rdb.IsChecked = false;
+                                find_rdb.Content = answer.answer;
+                                find_rdb.Tag = answer.checkAnswer;
+                            }                            
+                            break;
+                        case 2:
+                            var find_check = CheckStack.FindName($"check_{answerNumber + 1}") as CheckBox;
+                            if(find_check != null)
+                            {
+                                find_check.IsChecked = false;
+                                find_check.Content = answer.answer;
+                                find_check.Tag = answer.checkAnswer;
+                            }                            
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                var answer = Answers[0];
+                var find_txtbox = TextStack.FindName($"AnswerTextBox") as TextBox;
+                if (find_txtbox != null)
+                {
+                    find_txtbox.Text = "";
+                    find_txtbox.Tag = answer.checkAnswer;
+                }
+            }
         }
         public EcpTestView()
         {
