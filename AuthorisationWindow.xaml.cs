0b81574f5a04878faa5cdf78d59337043ed147af
Денис Шолопин <denis.sholopin@yandex.ru>
2023-05-15 17:29:07 +0300

    Закрепил картинку

--- AuthorisationWindow.xaml.cs
+++ AuthorisationWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace DiplomaProject
         {
             InitializeComponent();
         }
+
+        private void AuthCloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
